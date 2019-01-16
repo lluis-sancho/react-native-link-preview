5b71bb0f51f200cc8397219ab219114fc22b1b7d
lluis-sancho <lluissancho@gmail.com>
2019-01-16 22:41:27 +0100

    double regex

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ exports.getPreview = function(text, options) {
     var detectedUrl = null;
 
     text.replace(/\n/g, ' ').split(' ').forEach(function(token) {
-      if (CONSTANTS.REGEX_VALID_URL.test(token) && !detectedUrl) {
+      if ( (CONSTANTS.REGEX_VALID_URL.test(token) || CONSTANTS.REGEX_VALID_URL2.test(token)) && !detectedUrl) {
         detectedUrl = token;
       }
     });
